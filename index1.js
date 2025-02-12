9cc553468e80113b4b054131136a25f7b5e436be
Armaan253-code <am5432107@gmail.com>
2025-02-13 00:20:12 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,37 @@
+// //import{x} from './app'// This is problem of node js when we used import and export
+// const app=require('./app')
+// console.log(app.x);
+// console.log(app.y);
+// // var x=20;
+// // if(x===20){
+// //     console.log("matched")
+// // }
+
+// // for(i=0;i<10;i++){
+// //     console.log(i);
+// // }
+
+// const arr=[2,4,6,7,1,2,3,3,3,4];
+// console.log(arr);
+// console.log(app);
+// console.log(app.z());
+
+// //filter for interview question
+
+// let result=arr.filter((item)=>{
+//    // console.log(item);
+//     return item===3
+
+// })
+
+// console.warn(result);
+// const fs=require('fs');// const fs=require('fs').writeFileSync;
+// console.log("code step by step");
+// fs.writeFileSync("hello.txt","code step by step");
+// fs.writeFileSync("hello2.txt","like and subscribe");
+// console.log("=>>"+__dirname);
+// console.log("=>>"+filename);
+// const gs = require('fs').writeFileSync;
+// gs("xyz.txt", "abc");
+const gs = require('fs').writeFileSync;
+gs("xyz.txt", "abc");
